b7dba7579270328a1fc6f758e283098b6cd9cf02
Sema <jazz-dev@jazz-soft.net>
2025-08-04 22:55:09 -0400

    server

---
+++ server.js
@@ -0,0 +1,50 @@
+const os = require('os');
+const http = require('http');
+const openurl = require('openurl');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.set('views', path.join(__dirname, '/content'));
+app.use(express.static(path.join(__dirname, '/content')));
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var port = process.argv[2] || 8888;
+app.set('port', port);
+const server = http.createServer(app);
+server.on('listening', function() {
+  console.log(`http://${myIP()}:${port}`);
+  openurl.open(`http://localhost:${port}`);
+});
+
+server.listen(port);
+
+function myIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family == 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return 'localhost';
+}
