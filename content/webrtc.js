961cd876a3271aa2d83185300766667c03c9db66
Sema <jazz-dev@jazz-soft.net>
2025-08-17 23:04:31 -0400

    WebRTC seems to work

--- content/webrtc.js
+++ content/webrtc.js
@@ -2,13 +2,14 @@ function WebRtc() {
   var self = this;
   this.sig = new Signaling();
   this.rtc = new RTCPeerConnection();
-  this.sig.onfound = function() { self.start(); };
+  this.sig.onfound = function(x) { self.start(x); };
   this.sig.onrtc = function(x) { self.onrtc(x); };
   this.rtc.onicecandidate = function(x) { self.onicecandidate(x); };
   this.rtc.onnegotiationneeded = function() { self.onnegotiationneeded(); };
 }
 
-WebRtc.prototype.start = function() {
+WebRtc.prototype.start = function(x) {
+  if (x != 'polite') return;
   var self = this;
   self.rtc.createOffer().then(function(offer) {
     return self.rtc.setLocalDescription(offer);
@@ -18,13 +19,21 @@ WebRtc.prototype.start = function() {
 }
 
 WebRtc.prototype.onrtc = function(x) {
-  console.log('RTC:', x);
+  //console.log('RTC:', x);
   var self = this;
   if (x.offer) {
     const desc = new RTCSessionDescription(x.offer);
     self.rtc.setRemoteDescription(desc).then(function() {
       return self.rtc.createAnswer();
+    }).then(function(answer) {
+      return self.rtc.setLocalDescription(answer);
+    }).then(function() {
+      self.sig.rtc({ answer: self.rtc.localDescription });
     });
+  }
+  else if (x.answer) {
+    const desc = new RTCSessionDescription(x.answer);
+    self.rtc.setRemoteDescription(desc);
   }
   else if (x.candidate) {
     const candidate = new RTCIceCandidate(x.candidate);
@@ -37,7 +46,13 @@ WebRtc.prototype.onicecandidate = function(x) {
 }
 
 WebRtc.prototype.onnegotiationneeded = function() {
-  console.log('onnegotiationneeded');
+  console.log('negotiationneeded');
+  var self = this;
+  self.rtc.createOffer().then(function(offer) {
+    return self.rtc.setLocalDescription(offer);
+  }).then(function() {
+    self.sig.rtc({ offer: self.rtc.localDescription });
+  });
 }
 
 function error(e) { console.log('Error:', e.message); }
